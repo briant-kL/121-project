ff4f362eebd1c3d2b8a9c941010e3ca6c22633e2
briant-kL <44106641+briant-kL@users.noreply.github.com>
2019-03-05 15:21:30 -0800

    a lot re works to the code
    -Movement/Inventory
    -camera

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,11 +6,6 @@ using UnityEngine.UI;
 public class playerMovement : MonoBehaviour
 {
     
-    //References for player controller
-    /*https://medium.com/ironequal/unity-character-controller-vs-rigidbody-a1e243591483
-     * 
-     * Standard Assets first person controller c# file
-    */
 
 
     //Public variables
@@ -18,8 +13,23 @@ public class playerMovement : MonoBehaviour
     public float Gravity = -9.8f;
     public Vector3 drag;
     public float sprint;
+    public float _jumpheight;
     public GameObject ragdoll;
+    public GameObject _liteAtk_Hitbox;
+    public GameObject _hvyAtk_Hitbox;
+    public int health;
 
+    public float distToGround = 1.0f;
+
+
+
+
+    //inventory
+    public GameObject weapon;
+    public bool haveWeapon = false;
+    public bool key1 = false;
+    public bool key2 = false;
+    public bool key3 = false;
 
     //private variables
     private CharacterController playerController;
@@ -31,13 +41,14 @@ public class playerMovement : MonoBehaviour
     private ParticleSystem particles;
     private float _defaultSpeed;
 
-
+    private float _heavyFrame;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         playerController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
         particles = GetComponent<ParticleSystem>();
         
         _defaultSpeed = speed;
@@ -51,39 +62,107 @@ public class playerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         PlayerMovement();
-        Attack();
+
+        LightAttack();
+        HeavyAttack();
+
+       
+        
+        Roll();
+
     }
 
 
 
 
-    void Attack()
+    void LightAttack()
     {
         
         if (Input.GetMouseButtonDown(0))
         {
-            animator.SetBool("attacking", true);
-            animator.SetInteger("condition", 4);
-            speed = speed/2;
-            AttackRoutine();
-        }
-        
-        if (Input.GetMouseButtonUp(0))
-        {
-            animator.SetBool("attacking", false);
-            animator.SetInteger("condition", 0);
-            speed = _defaultSpeed;
+            //animator.SetBool("attacking", true);
+            //animator.SetInteger("condition", 4);
+
+            StartCoroutine(liteAttackRoutine());
+
         }
 
         
     }
 
-    IEnumerator AttackRoutine()
+    void HeavyAttack()
     {
-        yield return new WaitForSeconds(1);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            //animator.SetBool("attacking", true);
+            //animator.SetInteger("condition", 4);
+
+            StartCoroutine(hvyAttackRoutine());
+
+        }
         
+
+
     }
+
+
+    private int leftTotal;
+
+    void Roll()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        movement = new Vector2(h, v);
+        //These lines of code change the rotation of the player object so that it can walk in the right the direction according to the orientation of the camera
+        Vector3 moveInput = transform.forward * movement.y + transform.right * movement.x;
+        movement_direction.x = moveInput.x * speed;
+        movement_direction.z = moveInput.z * speed;
+        playerController.Move(movement_direction * Time.deltaTime);
+
+
+
+        if (Input.GetKeyDown("c"))
+        {
+            StartCoroutine(RollRoutine());
+        }
+
+
+
+    }
+
+
+    IEnumerator hvyAttackRoutine()
+    {
+        Debug.Log("heavy Attack");
+        _hvyAtk_Hitbox.SetActive(true);
+        yield return new WaitForSeconds(2);
+        _hvyAtk_Hitbox.SetActive(false);
+        Debug.Log("done");
+    }
+
+    IEnumerator liteAttackRoutine()
+    {
+        Debug.Log("lite Attack");
+        _liteAtk_Hitbox.SetActive(true);
+        yield return new WaitForSeconds(1);
+        _liteAtk_Hitbox.SetActive(false);
+        Debug.Log("done");
+    }
+
+
+
+    IEnumerator RollRoutine()
+    {
+        Debug.Log("Rolling");
+        yield return new WaitForSeconds(1);
+        Debug.Log("done Rolling");
+    }
+
+
 
     void PlayerMovement()
     {
@@ -106,14 +185,14 @@ public class playerMovement : MonoBehaviour
         {
             //Debug.Log(v);
             //animator.SetBool("walking", true);
-            animator.SetInteger("condition", 1);
+            //animator.SetInteger("condition", 1);
 
         }
 
         else if (Input.GetKeyUp("w"))
         {
             //animator.SetBool("walking", false);
-            animator.SetInteger("condition", 0);
+            //animator.SetInteger("condition", 0);
 
         }
 
@@ -121,13 +200,13 @@ public class playerMovement : MonoBehaviour
         //Sprint
         if (Input.GetKey(KeyCode.LeftShift) )
         {
-            animator.SetBool("running", true);
+            //animator.SetBool("running", true);
             speed = sprint;
             
         }
         else if (Input.GetKeyUp(KeyCode.LeftShift))
         {
-            animator.SetBool("running", false);
+            //animator.SetBool("running", false);
             speed = _defaultSpeed;
         }
 
@@ -137,12 +216,14 @@ public class playerMovement : MonoBehaviour
         {
             //Debug.Log(v);
             //animator.SetBool("backwalking", true);
-            animator.SetInteger("condition", -1);
+
+            //animator.SetInteger("condition", -1);
         }
         else if (Input.GetKeyUp("s"))
         {
             //animator.SetBool("backwalking", false);
-            animator.SetInteger("condition", 0);
+
+            //animator.SetInteger("condition", 0);
 
         }
 
@@ -151,14 +232,14 @@ public class playerMovement : MonoBehaviour
         {
            // Debug.Log(v);
 
-            animator.SetInteger("condition", 1);
+            //animator.SetInteger("condition", 1);
 
         }
 
         else if (Input.GetKeyUp("a"))
         {
 
-            animator.SetInteger("condition", 0);
+            //animator.SetInteger("condition", 0);
 
         }
 
@@ -166,30 +247,41 @@ public class playerMovement : MonoBehaviour
         {
            // Debug.Log(v);
 
-            animator.SetInteger("condition", -1);
+            //animator.SetInteger("condition", -1);
 
         }
 
         else if (Input.GetKeyUp("d"))
         {
 
-            animator.SetInteger("condition", 0);
+            //animator.SetInteger("condition", 0);
 
         }
 
 
 
-
-
-
-
-
-        //check grounded
-        if (playerController.isGrounded)
+        if (Input.GetKeyDown("c"))
         {
-            
+            Debug.Log("Rolling");
+            movement_direction.x = 10;
+            //RollRoutine();
+        }
+
+        //Jumping
+        //Using Raycast to check Grounded
+        //Debug.Log(isGrounded());
+
+        if (isGrounded() == true)
+        {
+            //Debug.Log("grounded");
+            if (Input.GetKey("space"))
+            {
+                Debug.Log("jumping");
+                velocity.y += Mathf.Sqrt(_jumpheight * -2f * Gravity);
+            }
 
         }
+
 
         //Gravity and Drag physics
         velocity.y += Gravity * Time.deltaTime;
@@ -200,21 +292,73 @@ public class playerMovement : MonoBehaviour
 
     }
 
+    
+
+
+    bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, distToGround);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("kill_env"))
         {
-            gameObject.SetActive(false);
-            Debug.Log("You died");
+            //gameObject.SetActive(false);
+            Debug.Log("You have been hit");
+            health = health - 1;
+            Debug.Log(health);
+            //ragdoll.SetActive(true);
+        }
 
-            ragdoll.SetActive(true);
-        }
-        if (other.gameObject.CompareTag("coins"))
+
+
+        //Inventory collisions
+
+        if (other.gameObject.CompareTag("weapon"))
         {
-            Debug.Log("Collected a coin!");
+            Debug.Log("You have gotten a weapon");
+            weapon.SetActive(true);
             other.gameObject.SetActive(false);
+
         }
+
+        if (other.gameObject.CompareTag("healthPack"))
+        {
+            Debug.Log("You gotten some health");
+            health = health + 1;
+            Debug.Log(health);
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("key1"))
+        {
+            Debug.Log("key1 acquired");
+            key1 = true;
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("key2"))
+        {
+            Debug.Log("key2 acquired");
+            key2 = true;
+            other.gameObject.SetActive(false);
+
+        }
+
+        if (other.gameObject.CompareTag("key3"))
+        {
+            Debug.Log("key3 acquired");
+            key3 = true;
+            other.gameObject.SetActive(false);
+
+        }
+
     }
+
+    
 
     
 
